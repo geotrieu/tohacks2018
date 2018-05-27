e0007d3ba5c97fffdf77e386887375db29463468
Dexter <deryanfloreza@outlook.com>
2018-05-26 20:44:41 -0400

    Update SOILHUMID.ino

--- TOHacks2018ESP/SOILHUMID.ino
+++ TOHacks2018ESP/SOILHUMID.ino
@@ -1,9 +1,13 @@
 void checkWater() {
+  // Pulls data from the humidity sensor 
   output_value= analogRead(sensor_pin);
   //Serial.println(output_value);
+  // Checks if there is water 
   if (output_value <= 400) {
+    // Turn LED on
     digitalWrite(ledPin, HIGH);
   } else {
+    // Turn the LED off
     digitalWrite(ledPin, LOW);
   }
 }
